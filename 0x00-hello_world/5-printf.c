b99a06bfbebfa4e3c91ba2935900cf167883b02b
NaGu-Tech <agu.chikaodiri.ng@gmail.com>
2022-10-13 14:07:12 -0700

    task5 for 0x00-low_level_programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  *main - This prints the texts for the task
+  *
+  *Return: This return 0 for a successful exercusion
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
