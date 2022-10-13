ce4ae087584fd984f4900b61a20616d62fe7a05a
NaGu-Tech <agu.chikaodiri.ng@gmail.com>
2022-10-13 16:29:11 -0700

    task8 for 0x00-low_level_programming

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+  *main - The point of program entry
+  *
+  *Return: Prints 1 on successful exercusion
+  */
+int main(void)
+{
+	write(2, "and that piece of art is usefull\" - Dora Korar, 2015-10-19\n", 61);
+	return (1);
+}
