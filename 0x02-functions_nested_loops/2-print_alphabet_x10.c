875b533f65f5c18e4b9cc705337f6ce22a0d9e8a
NaGu-Tech <agu.chikaodiri.ng@gmail.com>
2022-10-18 14:07:13 -0700

    prints alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  *print_alphabet_x10 -> prints the lowercase alphabet
+  */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int x;
+
+	for (x = 0; x < 10; z++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
