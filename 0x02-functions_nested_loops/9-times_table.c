fd6d9897cd251800fca5d244004fd42e99d52be9
NaGu-Tech <agu.chikaodiri.ng@gmail.com>
2022-10-18 18:40:31 -0700

    tsk 9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -25,6 +25,7 @@ void times_table(void)
 
 			{
 				_putchar((d / 10) + '0');
+			}
 
 				else
 				{
@@ -37,7 +38,7 @@ void times_table(void)
 					_putchar(',');
 					_putchar(' ');
 				}
-			}
+
 			_putchar('\n');
 		}
 	}
