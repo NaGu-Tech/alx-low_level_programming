b81d7ef2eb1ca0fe042946f1a92a18b33fd27067
NaGu-Tech <agu.chikaodiri.ng@gmail.com>
2022-10-18 17:32:02 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  *print_last_digit -> print the last digit
+  *@n: print_last_digit
+  *Returns: value of print_last_digit
+  */
+int print_last_digit(int n);
+{
+	int x;
+
+	x = (n % 10);
+
+	if (x < 0)
+	{
+		x = (-1 * x);
+	}
+	_putchar(x + '0');
+	return (x);
+}
