0075e99337d85d9617204c15d210ad8e76bcbe71
NaGu-Tech <agu.chikaodiri.ng@gmail.com>
2022-10-20 04:06:32 -0700

    fit hot task 1 debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,13 +1,14 @@
 #include <stdio>
 
 /**
- *main - causes an infinite loop
-Return: 0
+*main - causes an infinite loop
+*Return: 0
 */
 
 int main(void)
 {
 	int i;
+
 	printf("infinite loop incoming: (\n");
 
 	i = 0;
@@ -17,6 +18,8 @@ int main(void)
 	 *putchar (i);
 	 *}
 	 */
+
 	printf("infinite loop avoided! \\0/\n");
+
 	return (0);
 }
