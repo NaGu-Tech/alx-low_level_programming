bc29144d94d6cffa53a527d649fd28a21be09cf8
NaGu-Tech <agu.chikaodiri.ng@gmail.com>
2022-10-20 13:59:04 -0700

    print more numbers fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,11 +7,15 @@ void more_numbers(void)
 {
 	int x, y;
 
-	for (x = 0; x < 10; x++)
+	for (y = 0; y < 10; y++)
 	{
-		for (y = 0; y < 15; y++)
+		for (x = 0; x <= 14; x++)
 		{
-			_putchar(y + '0');
+			if (x >= 10)
+			{
+			_putchar((x / 10) + '0');
+			}
+			_putchar((x % 10) + '0');
 		}
 		_putchar('\n');
 	}
