081fd11f37b03f6931394fe0eaa5b7f08636e214
NaGu-Tech <agu.chikaodiri.ng@gmail.com>
2022-10-20 19:09:38 -0700

    advancd task'

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *print_number - prints an integer
+  *@n: The integer to print
+  *
+  *Return: nothing
+  */
+
+void print_number(int n)
+{
+	int n;
+
+	return (0);
+}
