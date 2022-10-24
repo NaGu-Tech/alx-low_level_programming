9407dc1f3c8d0e740e3e9c0a9cdf57ebceda2971
NaGu-Tech <agu.chikaodiri.ng@gmail.com>
2022-10-24 15:23:03 -0700

    tasks for 0x05 c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,15 +3,21 @@
 /**
   *print_rev - prints a string in reverse
   *@s: string to print
+  *
+  *Return: no return
   */
 void print_rev(char *s)
 {
-	int len = 0, index = 0;
+	int count = 0;
 
-	len = _strlen(s);
-
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+			break;
+		count++;
+	}
+	for (count--; count >= 0; count--)
+		_putchar(s[count]);
 
 	_putchar('\n');
 }
