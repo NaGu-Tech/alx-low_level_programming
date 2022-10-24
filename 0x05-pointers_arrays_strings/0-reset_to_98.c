e9cd5fcb86ec6a1d4ecfef564a024fa32dbbdc5f
NaGu-Tech <agu.chikaodiri.ng@gmail.com>
2022-10-24 12:49:44 -0700

    task 0 for 0x05 c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+  *reset_to_98 -Resets the value of a variable to 98
+  *@n: Variable to be reset
+  *Return: returns void on success
+  */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
