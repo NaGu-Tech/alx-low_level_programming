9928c7309e0d959b447c4a1fc8ee7552ade0c4ef
NaGu-Tech <agu.chikaodiri.ng@gmail.com>
2022-11-04 06:14:50 -0700

    task for 0x0A-argc_argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,71 +1,51 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-/**
- * _isnumber - checks if string is a number
- * @s: string
- *
- * Return: On success 1.
- * If not a number, 0 is returned.
- */
-int _isnumber(char *s)
-{
-	int i, check, d;
-
-	i = 0, d = 0, check = 1;
-	if (*s == '-')
-		i++;
-	for (; *(s + i) != 0; i++;)
-	{
-		d = isdigit(*(s + i));
-		if (d == 0)
-		{
-			check = 0;
-			break;
-		}
-	}
-	return (check);
-}
 /**
  * main - Entry point
  *
- * @argc: Counts the number of parameters that go into main
- * @argv: Pointer of array of pointers containing strings entering main
- * Return: Always 0 (Success)
+ * Description: prints the minimum number of coins to make change for an amount
+ *of money
+ * @argc: amount of arguments passed through the program
+ * @argv: pointer that contains the arrays of the arguments
+ *
+ * Return: the minimum number of coins needed to make the change for the amount
+ *of money required
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int j, ex, coins, cents, d;
-	int c[5] = {25, 10, 5, 2, 1};
+	int x = 0, money = 0;
 
-	ex = 1, j = 0, coins = 0;
-	if (argc == 2)
+	if (argc != 2)
 	{
-		if (_isnumber(argv[1]))
-		{
-			ex = 0, cents = atoi(argv[1]);
-			if (cents >= 0)
-			{
-				while (cents != 0)
-				{
-					d = cents / c[j];
-					if (d == 0)
-					{
-						j++;
-					}
-					else
-					{
-						coins += d;
-						cents -= (d * c[j])
-					}
-				}
-			}
-		}
+		printf("Error\n");
+		return (1);
 	}
-		if (ex == 0)
-			printf("%i\n", coins);
-		else
-			printf("%s\n", "Error");
-		return (ex);
+	money = atoi(argv[1]);
+	while (money > 0)
+	{
+		if (money - 25 >= 0)
+		{
+			money = money - 25;
+		}
+		else if (money - 10 >= 0)
+		{
+			money = money - 10;
+		}
+		else if (money - 5 >= 0)
+		{
+			money = money - 5;
+		}
+		else if (money - 2 >= 0)
+		{
+			money = money - 2;
+		}
+		else if (money - 1 == 0)
+		{
+			money = money - 1;
+		}
+		x++;
+	}
+	printf("%d\n", x);
+	return (0);
 }
