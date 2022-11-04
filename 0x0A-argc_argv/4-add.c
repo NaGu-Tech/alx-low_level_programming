9e7015b65e66f100482fc97beeb418a6e3ad0c86
NaGu-Tech <agu.chikaodiri.ng@gmail.com>
2022-11-04 05:18:19 -0700

    task for 0x0A-argc_argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,7 +8,7 @@
   * @argc: amount of arguments passed through the program
   * @argv: pointer that contains the arrays of the arguments
   *
-  * @argv: pointer that contains the arrays of the arguments a
+  * Return: the sum of the numbers, if no number is passed then print 0, if a
   *number has symbols that are not digit then print Error and return 1
   */
 int main(int argc, char *argv[])
