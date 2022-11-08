0db6675e1ab1429be234724505ffa59ca0a21cf4
NaGu-Tech <agu.chikaodiri.ng@gmail.com>
2022-11-08 03:18:48 -0800

    task for 0x0B-malloc_free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,13 +3,13 @@
 #include <stdlib.h>
 
 /**
-  *alloc_grid - creates a 2D array and returns the pointer to the memory
-  *
-  *@width: width of the 2D array
-  *@height: height of the 2D array
-  *
-  *Return: a pointer to the 2D array or NULL if the process fails
-  */
+ * alloc_grid - creates a 2D array and returns the pointer to the memory
+ *
+ *@width: width of the 2D array
+ *@height: height of the 2D array
+ *
+ *Return: a pointer to the 2D array or NULL if the process fails
+ */
 int **alloc_grid(int width, int height)
 {
 	int **grid, i, j;
@@ -18,7 +18,7 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	grid = malloc(sizeof(int *) * height)if (grid == NULL);
+	grid = malloc(sizeof(int *) * height);
 	if (grid == NULL)
 	{
 		return (NULL);
