9f604588be99428ad7c50e08fc62d4a04a65909b
NaGu-Tech <agu.chikaodiri.ng@gmail.com>
2022-11-07 23:40:13 -0800

    task for 0x0B-malloc_free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,6 +3,41 @@
 #include <stdlib.h>
 
 /**
+  * _strlen - return length of a string
+  *
+  *@s: char type
+  *Return: length of string
+  */
+
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+		;
+
+	return (a);
+}
+
+/**
+  * _strncat - function to concatenate strings with n bytes
+  *
+  * @dest: destination  for concatenation
+  * @src: souce of string
+  * @n: int type for size of byte
+  * Return: dest
+  */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len, a;
+
+	dest_len = _strlen(dest);
+	for (a = 0; a < n && src[a] != '\0'; a++)
+		dest[dest_len + a] = src[a];
+	return (dest);
+}
+
+/**
   * *str_concat - function to allocate space for string concatenation
   *
   * @s1: array pointer to destination of string
