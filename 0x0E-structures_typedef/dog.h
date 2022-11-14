99c9171b6cfaefb56dcc6f6f58f7568d041d5fc3
NaGu-Tech <agu.chikaodiri.ng@gmail.com>
2022-11-14 04:34:28 -0800

    task for 0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * @struct dog - creates a struct for dogs' information
+  * @name: dog's name
+  * @age: dog's age
+  * @owner: dog's owner
+  *
+  *Description: Long descriotion
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog2;
+dog2 *new_dog(char *name, float age, char *owner);
+void free_dog(dog2 *d);
+
+#endif
