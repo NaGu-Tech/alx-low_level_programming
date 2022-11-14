1c7183c222bf099b39e080768800ffa62e659bb8
NaGu-Tech <agu.chikaodiri.ng@gmail.com>
2022-11-14 05:37:55 -0800

    task for 0x0E-structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,8 +18,8 @@ struct dog
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog dog2;
-dog2 *new_dog(char *name, float age, char *owner);
-void free_dog(dog2 *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
