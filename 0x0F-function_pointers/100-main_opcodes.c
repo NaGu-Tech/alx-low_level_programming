533162ff6343b1a2a40b73ccb4db57ac8c0d41ea
NaGu-Tech <agu.chikaodiri.ng@gmail.com>
2022-11-16 04:41:50 -0800

    task for 0x0F-function_pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -26,7 +26,7 @@ int main(int argc, char **argv)
 		exit(2);
 	}
 	ptr = &main;
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n; ++i)
 		printf("%.2x", *((unsigned char *)(ptr + i)));
 	printf("\n");
 	return (0);
